ba63d41e57b0c7d0265e32a2c05c9185de19c199
Manuel <manuel.baden@gmx.de>
2023-03-16 20:54:25 +0100

    Add comparator for flush including first test

---
+++ src/main/java/comparators/FlushComparator.java
@@ -0,0 +1,11 @@
+package comparators;
+
+import model.Hand;
+
+public class FlushComparator implements HandComparator {
+
+    @Override
+    public int compare(Hand firstHand, Hand secondHand) {
+        return 0;
+    }
+}
---
+++ src/test/java/comparators/FlushComparatorTest.java
@@ -0,0 +1,36 @@
+package comparators;
+
+import model.Card;
+import org.junit.jupiter.api.Test;
+
+import static model.CardSuit.*;
+import static model.CardValue.*;
+import static model.Hand.createHand;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class FlushComparatorTest {
+
+    private final FlushComparator testee = new FlushComparator();
+
+    @Test
+    void compare_shouldReturnTie_whenBothHandsAreNotFlush() {
+        var firstHand = createHand(
+                new Card(CLUBS, ACE),
+                new Card(SPADES, TEN),
+                new Card(DIAMONDS, JACK),
+                new Card(HEARTS, SEVEN),
+                new Card(HEARTS, JACK)
+        );
+        var secondHand = createHand(
+                new Card(DIAMONDS, TWO),
+                new Card(SPADES, JACK),
+                new Card(HEARTS, SIX),
+                new Card(DIAMONDS, EIGHT),
+                new Card(SPADES, QUEEN)
+        );
+
+        var result = testee.compare(firstHand, secondHand);
+
+        assertThat(result).isEqualTo(0);
+    }
+}
