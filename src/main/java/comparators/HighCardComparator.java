8d453fbe6a6318e1782d42b2c398fdad511c74c8
Manuel <manuel.baden@gmx.de>
2023-03-16 21:44:49 +0100

    Introduce HighCardComparator

---
+++ src/main/java/comparators/HighCardComparator.java
@@ -0,0 +1,11 @@
+package comparators;
+
+import model.Hand;
+
+public class HighCardComparator implements HandComparator {
+
+    @Override
+    public int compare(Hand firstHand, Hand secondHand) {
+        return 0;
+    }
+}
