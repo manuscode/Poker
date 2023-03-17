53856746547423ec4a85cea80fa53bdb2950626e
Manuel <manuel.baden@gmx.de>
2023-03-17 18:02:19 +0100

    Refactor HighCardComparator, allow comparison of lists of cards

--- src/main/java/comparators/HighCardComparator.java
+++ src/main/java/comparators/HighCardComparator.java
@@ -1,6 +1,9 @@
 package comparators;
 
+import model.Card;
 import model.Hand;
+
+import java.util.List;
 
 import static comparators.ComparisonResult.*;
 import static utils.HandUtil.sortedCardListByRank;
@@ -9,12 +12,16 @@ public class HighCardComparator implements HandComparator {
 
     @Override
     public ComparisonResult compare(Hand firstHand, Hand secondHand) {
-        var firstSortedCardList = sortedCardListByRank(firstHand);
-        var secondSortedCardList = sortedCardListByRank(secondHand);
+        return compareCardLists(firstHand.getCardList(), secondHand.getCardList());
+    }
 
-        for (var index = 0; index < firstSortedCardList.size(); index++) {
-            var firstCardRank = firstSortedCardList.get(index).getValue().getRank();
-            var secondCardRank = secondSortedCardList.get(index).getValue().getRank();
+    public ComparisonResult compareCardLists(List<Card> firstHandCardList, List<Card> secondHandCardList) {
+        var firstHandSortedCardList = sortedCardListByRank(firstHandCardList);
+        var secondHandSortedCardList = sortedCardListByRank(secondHandCardList);
+
+        for (var index = 0; index < firstHandSortedCardList.size(); index++) {
+            var firstCardRank = firstHandSortedCardList.get(index).getValue().getRank();
+            var secondCardRank = secondHandSortedCardList.get(index).getValue().getRank();
 
             if (firstCardRank > secondCardRank) {
                 return FIRST_HAND_WIN;
