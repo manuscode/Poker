a8032812d6f2819cecd7c5f3f29e34a20915a069
Manuel <manuel.baden@gmx.de>
2023-03-17 21:15:06 +0100

    Add ThreeOfAKindComparator

---
+++ src/main/java/comparators/ThreeOfAKindComparator.java
@@ -0,0 +1,48 @@
+package comparators;
+
+import lombok.AllArgsConstructor;
+import model.CardValue;
+import model.Hand;
+
+import java.util.Map;
+import java.util.Optional;
+
+import static comparators.ComparisonResult.*;
+import static utils.HandUtil.getCardsByValue;
+
+@AllArgsConstructor
+public class ThreeOfAKindComparator implements HandComparator {
+
+    private final HighCardComparator highCardComparator;
+
+    @Override
+    public ComparisonResult compare(Hand firstHand, Hand secondHand) {
+        var firstHandThreeOfAKindValue = extractThreeOfAKindValue(firstHand);
+        var secondHandThreeOfAKindValue = extractThreeOfAKindValue(secondHand);
+
+        var firstHandHasThreeOfAKind = firstHandThreeOfAKindValue.isPresent();
+        var secondHandHasThreeOfAKind = secondHandThreeOfAKindValue.isPresent();
+
+        if (firstHandHasThreeOfAKind && secondHandHasThreeOfAKind) {
+            return highCardComparator.compareValues(
+                    firstHandThreeOfAKindValue.get(),
+                    secondHandThreeOfAKindValue.get()
+            );
+        }
+        if (firstHandHasThreeOfAKind) {
+            return FIRST_HAND_WIN;
+        }
+        if (secondHandHasThreeOfAKind) {
+            return SECOND_HAND_WIN;
+        }
+
+        return TIE;
+    }
+
+    private static Optional<CardValue> extractThreeOfAKindValue(Hand hand) {
+        return getCardsByValue(hand).entrySet().stream()
+                .filter(entry -> entry.getValue().size() == 3)
+                .findFirst()
+                .map(Map.Entry::getKey);
+    }
+}
---
+++ src/test/java/comparators/ThreeOfAKindComparatorTest.java
@@ -0,0 +1,119 @@
+package comparators;
+
+import model.Card;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
+
+import static comparators.ComparisonResult.*;
+import static model.CardSuit.*;
+import static model.CardValue.*;
+import static model.Hand.createHand;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+class ThreeOfAKindComparatorTest {
+
+    private final HighCardComparator highCardComparator = mock(HighCardComparator.class);
+
+    private final ThreeOfAKindComparator testee = new ThreeOfAKindComparator(highCardComparator);
+
+    @Test
+    void compare_shouldReturnTie_whenNoHandHasThreeOfAKind() {
+        var firstHand = createHand(
+                new Card(CLUBS, ACE),
+                new Card(SPADES, TEN),
+                new Card(DIAMONDS, JACK),
+                new Card(HEARTS, TWO),
+                new Card(HEARTS, THREE)
+        );
+        var secondHand = createHand(
+                new Card(DIAMONDS, TWO),
+                new Card(SPADES, TEN),
+                new Card(HEARTS, SIX),
+                new Card(DIAMONDS, SEVEN),
+                new Card(SPADES, ACE)
+        );
+
+        var result = testee.compare(firstHand, secondHand);
+
+        assertThat(result).isEqualTo(TIE);
+
+    }
+
+    @Test
+    void compare_shouldReturnFirstHandWin_whenOnlyFirstHandHasThreeOfAKind() {
+        var firstHand = createHand(
+                new Card(CLUBS, ACE),
+                new Card(SPADES, ACE),
+                new Card(DIAMONDS, JACK),
+                new Card(HEARTS, TWO),
+                new Card(HEARTS, ACE)
+        );
+        var secondHand = createHand(
+                new Card(DIAMONDS, TWO),
+                new Card(SPADES, TEN),
+                new Card(HEARTS, SIX),
+                new Card(DIAMONDS, SEVEN),
+                new Card(SPADES, ACE)
+        );
+
+        var result = testee.compare(firstHand, secondHand);
+
+        assertThat(result).isEqualTo(FIRST_HAND_WIN);
+
+    }
+
+    @Test
+    void compare_shouldReturnSecondHandWin_whenOnlySecondHandHasThreeOfAKind() {
+        var firstHand = createHand(
+                new Card(CLUBS, ACE),
+                new Card(SPADES, QUEEN),
+                new Card(DIAMONDS, JACK),
+                new Card(HEARTS, TWO),
+                new Card(HEARTS, ACE)
+        );
+        var secondHand = createHand(
+                new Card(DIAMONDS, SEVEN),
+                new Card(SPADES, TEN),
+                new Card(HEARTS, SIX),
+                new Card(DIAMONDS, SEVEN),
+                new Card(SPADES, SEVEN)
+        );
+
+        var result = testee.compare(firstHand, secondHand);
+
+        assertThat(result).isEqualTo(SECOND_HAND_WIN);
+
+    }
+
+    @ParameterizedTest
+    @EnumSource(ComparisonResult.class)
+    void compare_shouldReturnResultOfHighCardComparator_whenBothHandsHaveThreeOfAKind(
+            ComparisonResult comparisonResult
+    ) {
+        var firstHand = createHand(
+                new Card(CLUBS, ACE),
+                new Card(SPADES, QUEEN),
+                new Card(DIAMONDS, ACE),
+                new Card(HEARTS, TWO),
+                new Card(HEARTS, ACE)
+        );
+        var secondHand = createHand(
+                new Card(DIAMONDS, SEVEN),
+                new Card(SPADES, TEN),
+                new Card(HEARTS, SIX),
+                new Card(DIAMONDS, SEVEN),
+                new Card(SPADES, SEVEN)
+        );
+
+        when(highCardComparator.compareValues(eq(ACE), eq(SEVEN))).thenReturn(comparisonResult);
+
+        var result = testee.compare(firstHand, secondHand);
+
+        assertThat(result).isEqualTo(comparisonResult);
+
+    }
+}
